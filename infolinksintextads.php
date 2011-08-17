14ea21d408238093e76c97859d0c3bbb9389e200
Infolinks Official Plugin <Infolinks Official Plugin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-17 14:06:07 +0000

    Committing v3.0 of the Infolinks plugin
    git-svn-id: https://plugins.svn.wordpress.org/infolinks-officlial-plugin/trunk@424848 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ infolinksintextads.php
@@ -0,0 +1,327 @@
+<?php
+/*
+Plugin Name: Infolinks Official Plugin
+Plugin URI: http://www.infolinks.com
+Description: This plugin will automatically add your Infolinks script to your website pages.
+Author: Infolinks
+Version: 3.0
+Author URI: http://www.infolinks.com
+*/
+
+//install/uninstall function calls
+register_activation_hook( __FILE__, 'infolinks_install' );
+register_uninstall_hook( __FILE__, 'infolinks_uninstall' );
+
+add_action( 'admin_menu', 'infolinks_pages' );
+add_action( 'wp_footer', 'infolinksAdsscript' );
+
+function infolinks_install() {
+	add_option( 'infolinks_status', '1' );
+	add_option( 'infolinks_comments', '1' );
+	add_option( 'infolinks_post', '1' );
+}
+
+function infolinks_uninstall() {
+	delete_option( 'infolinks_status' );
+	delete_option( 'infolinks_publisherid' );
+	delete_option( 'infolinks_websiteid' );
+	delete_option( 'infolinks_excludepage' );
+	delete_option( 'infolinks_comments' );
+	delete_option( 'infolinks_post' );
+	delete_option( 'infolinks_keytag' );
+	delete_option( 'infolinks_jquery' );
+}
+
+$pluginpath = plugins_url( '/', __FILE__ );
+
+define( 'JS_PATH', $pluginpath .'js/jquery.js' );
+
+// action function for above hook
+function infolinks_pages() {
+    add_options_page( 'Info Links Text Ads', 'Infolinks Settings', 'manage_options', 'infolink-admin', 'infosettingoptions_page' );
+}
+
+//plugin settings page
+function infosettingoptions_page() {
+
+	//save plugin settings
+	if( isset( $_POST['btnSave'] ) ) {
+
+		//check nonce for security
+		check_admin_referer( 'infolinks_plugin_save' );
+
+		$infolinks_status = ( isset( $_POST['infolinks_status'] ) ) ? $_POST['infolinks_status'] : 0;
+		$infolinks_keytag = ( isset( $_POST['infolinks_keytag'] ) ) ? $_POST['infolinks_keytag'] : 0;
+		$infolinks_jquery = ( isset( $_POST['infolinks_jquery'] ) ) ? $_POST['infolinks_jquery'] : 0;
+		$infolinks_comments = ( isset( $_POST['infolinks_comments'] ) ) ? $_POST['infolinks_comments'] : 0;
+		$infolinks_post = ( isset( $_POST['infolinks_post'] ) ) ? $_POST['infolinks_post'] : 0;
+
+		update_option( 'infolinks_status', absint( $infolinks_status ) );
+		update_option( 'infolinks_publisherid', absint( $_POST['infolinks_publisherid'] ) );
+		update_option( 'infolinks_websiteid', absint( $_POST['infolinks_websiteid'] ) );
+		update_option( 'infolinks_excludepage', strip_tags( $_POST['infolinks_excludepage'] ) );
+		update_option( 'infolinks_comments', absint( $infolinks_comments ) );
+		update_option( 'infolinks_post', absint( $infolinks_post ) );
+		update_option( 'infolinks_keytag', absint( $infolinks_keytag ) );
+		update_option( 'infolinks_jquery', absint( $infolinks_jquery ) );
+
+		echo '<div id="message" class="updated">Settings saved successfully</div>';
+
+	}
+
+	//reset plugin settings
+	if( isset( $_POST['btnReset'] ) ) {
+
+		//check nonce for security
+		check_admin_referer( 'infolinks_plugin_save' );
+
+		//set options back to specified defaults
+		update_option( 'infolinks_status', 1 );
+		update_option( 'infolinks_comments', 1 );
+		update_option( 'infolinks_post', 1 );
+		update_option( 'infolinks_keytag', 1 );
+		update_option( 'infolinks_jquery', 0 );
+		delete_option( 'infolinks_excludepage' );
+
+		echo '<div id="message" class="updated">Settings reset successfully</div>';
+	}
+
+	//load setting values
+	$infolinks_status = get_option( 'infolinks_status' );
+	$infolinks_keytag = get_option( 'infolinks_keytag' );
+	$infolinks_jquery = get_option( 'infolinks_jquery' );
+	$infolinks_publisherid = get_option( 'infolinks_publisherid' );
+	$infolinks_websiteid = ( get_option( 'infolinks_websiteid' ) ) ? get_option( 'infolinks_websiteid' ) : 0;
+	$infolinks_excludepage = get_option( 'infolinks_excludepage' );
+	$infolinks_post = get_option( 'infolinks_post' );
+	$infolinks_comments = get_option( 'infolinks_comments' );
+?>
+<style type="text/css">
+.small_txt {
+	font-size:0.85em;
+	color:#898989;
+	font-family:Verdana,sans-serif;
+}
+h2 {
+	font-family:Georgia,"Times New Roman","Bitstream Charter",Times,serif;
+	font-size:24px;
+	font-size-adjust:none;
+	font-stretch:normal;
+	font-style:italic;
+	font-variant:normal;
+	font-weight:normal;
+	line-height:35px;
+	margin:0;
+	padding:14px 15px 3px 0;
+	text-shadow:0 1px 0 #FFFFFF;
+}
+</style>
+<form method="post" name="frm_infolinks" id="frm_infolinks">
+	<?php wp_nonce_field( 'infolinks_plugin_save' ); ?>
+  <input type="hidden" id="infoid" name="infoid" value="" />
+  <div style="float:left; width:40%;"><table border="1" width="100%">
+    <tr>
+      <td><table width="95%">
+          <tr>
+            <td colspan="2"><h2>Infolinks Official Plugin</h2></td>
+          </tr>
+          <tr>
+            <td colspan="2">This plugin will automatically add your Infolinks script to your website pages</td>
+          </tr>
+          <tr>
+            <td colspan="2" height="30">&nbsp;</td>
+          </tr>
+          <tr>
+            <td valign="top" width="200">Infolinks In Text Ads:</td>
+			<td>
+				<input type="radio" name="infolinks_status" value="1" <?php checked( $infolinks_status, 1 ); ?> /> On <br />
+				<input type="radio" name="infolinks_status" value="0" <?php checked( $infolinks_status, 0 ); ?> /> Off
+			</td>
+          </tr>
+          <tr>
+            <td colspan="2" height="20">&nbsp;</td>
+          </tr>
+          <tr>
+            <td valign="top"><label for="publisherid">Publisher ID :</label></td>
+            <td>
+				<input type="text" name="infolinks_publisherid" id="infolinks_publisherid"  value="<?php echo esc_attr( $infolinks_publisherid ); ?>"/><br />
+				<span class="small_txt">Please enter your infolinks pid</span>
+			</td>
+          </tr>
+          <tr>
+            <td valign="top"><label for="websiteid">Website ID :</label></td>
+            <td>
+				<input type="text" name="infolinks_websiteid" id="infolinks_websiteid" value="<?php echo esc_attr( $infolinks_websiteid ); ?>"/><br />
+				<span class="small_txt">Please enter your website wsid</span>
+			</td>
+          </tr>
+          <tr>
+			<td colspan="2" height="40">&nbsp;</td>
+          </tr>
+          <tr>
+            <td colspan="2"><label for="excludepage">Exclude from the following pages</label>
+				<input type="text" name="infolinks_excludepage" id="infolinks_excludepage" value="<?php echo esc_attr( $infolinks_excludepage ); ?>" /><br />
+				<span class="small_txt"> (please enter page numbers comma- separated. Infolinks will not be added to these pages)</span>
+			</td>
+          </tr>
+          <tr>
+            <td colspan="2" height="20">&nbsp;</td>
+          </tr>
+          <tr>
+            <td valign="top">jQuery :</td>
+            <td><input type="radio" name="infolinks_jquery" value="1" <?php checked( $infolinks_jquery, 1 ); ?> /> Use plugin jQuery <br />
+              <input type="radio" name="infolinks_jquery" value="0"  <?php checked( $infolinks_jquery, 0 ); ?> /> Use site jQuery
+			</td>
+          </tr>
+		  <tr>
+            <td colspan="2" height="20">&nbsp;</td>
+          </tr>
+		  <tr>
+            <td valign="top">Show in header / titles: <br />
+			<span class="small_txt">(jQuery is mandatory for this feature)</span></td>
+            <td>
+				<input type="radio" name="infolinks_keytag" value="1" <?php checked( $infolinks_keytag, 1 ); ?> />On <br />
+				<input type="radio" name="infolinks_keytag" value="0"  <?php checked( $infolinks_keytag, 0 ); ?> />Off </td>
+          </tr>
+          <tr>
+            <td colspan="2" height="20">&nbsp;</td>
+          </tr>
+          <tr>
+			<td colspan="2">
+				<div style="padding-top:3px;float:left;">
+				<input type="checkbox" name="infolinks_post" value="1"  <?php checked( $infolinks_post, 1 ); ?> />
+                &nbsp;</div>
+              <div style="padding-top:3px;float:left;">Enable Infolinks on posts</div>
+			</td>
+          </tr>
+          <tr>
+            <td colspan="2">
+				<div style="padding-top:3px;float:left;">
+				<input type="checkbox" name="infolinks_comments" value="1" <?php checked( $infolinks_comments, 1 ); ?> />
+                &nbsp;</div>
+				<div style="padding-top:3px;float:left;">Enable Infolinks on comments</div>
+			</td>
+          </tr>
+          <tr>
+		     <td colspan="2" height="40">&nbsp;</td>
+          </tr>
+		  <tr>
+			  <td colspan="2">
+					<strong>Want to earn even more?</strong>
+					<p><a href="http://publishers.infolinks.com/signin/tag-cloud.html" target="_blank">Click here to activate Tag Cloud on your pages</a></p>
+					<p>By adding Tag Cloud to your pages, you can increase your earnings with our attractive, fully-customizable cloud of keywords that display in a Tag Cloud unit at the bottom of your text.</p>
+					<p>Tag Cloud operates just like our In-Text ads, a mouse hover reveals our ad bubble, and each click equals more money for you! Our smart algorithm delivers the best keywords for each page and helps to further turn your content into money by matching each keyword with a relevant ad.</p>
+			  </td>
+          <tr>
+		     <td colspan="2" height="40">&nbsp;</td>
+          </tr>
+        </table></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td colspan="2">To view or edit your Infolinks account settings, please visit the <a href="http://publishers.infolinks.com/signin/implementation.html" target="_blank">Integration guide</a> <br />
+        and <a href="http://www.infolinks.com/faq.html" target="_blank">our FAQs</a>, or contact us at <a href="mailto:support@infolinks.com">support@infolinks.com</a> </td>
+    </tr>
+    <tr>
+      <td colspan="2" height="20">&nbsp;</td>
+    </tr>
+    <tr>
+		<td colspan="2" height="40">
+			<input type="submit" class="button-primary" name="btnSave" value="Save Settings" />
+			<input type="submit" class="button-secondary" name="btnReset" value="Reset to Default" />
+		</td>
+    </tr>
+  </table>
+  </div>
+  <div style="float: left; margin-left:auto; padding-top:100px; width:60%;">
+	<img src="<?php echo plugins_url( 'images/bubble.png', __FILE__ ); ?>" width="350" style="padding-left:150px;" /><br />
+	<img src="<?php echo plugins_url( 'images/tag-cloud.png', __FILE__ ); ?>" width="550" style="padding-top:80px;" />
+  </div>
+
+</form>
+<?php	
+}
+
+//add the script in footer
+function infolinksAdsscript() {
+	$display = 0;
+	//to check the ads enable or disable
+	 $options = get_option( 'infolinks_status' );
+	 $tagKeys = get_option( 'infolinks_keytag' );
+	 $JQuery = get_option( 'infolinks_jquery' );
+	 $cJoin = '';
+	 $idJoin = '';
+	 $keyJoin = '';
+	 $strOnOff = '';
+
+	 if( $options == 1 ){
+			 $display =1;
+			 $postings = get_option( 'infolinks_post' );
+			 $commentspage = get_option( 'infolinks_comments' );
+			 $pages = get_option( 'infolinks_excludepage' );
+			 
+			   //to check the following pages
+			   if( !empty( $pages ) ) {
+			   		$disablepageArray = explode( ",", $pages );
+					global $post;
+					$current_page = $post->ID;	
+					foreach ( $disablepageArray as $page ) {
+						if ( $current_page == intval( $page ) ) {
+							$display = 0;
+						}
+					}
+			   }
+			  if (is_single() || is_front_page()) {  // to check the post
+			  		$display = 0;
+			    	 if( $postings == 1 ){
+						$display = 1;
+					 } 
+			   }
+
+			   if( $tagKeys == 0 ) {
+
+					$classArray = array( ".description" );
+					$IdArray = array( "#headerimg" );
+					$KeyArray = array( "h1", "h2", "h3" );
+					
+					for( $c=0; $c < count( $classArray ); $c++ ){
+						$cJoin .= "jQuery('".$classArray[$c]."').before('<span><!--INFOLINKS_OFF--></span>');";
+						$cJoin .= "jQuery('".$classArray[$c]."').after('<span><!--INFOLINKS_ON--></span>');";
+					}
+					
+					for( $i=0; $i < count( $IdArray ); $i++ ){
+						$idJoin .= "jQuery('".$IdArray[$i]."').before('<span><!--INFOLINKS_OFF--></span>');";
+						$idJoin .= "jQuery('".$IdArray[$i]."').after('<span><!--INFOLINKS_ON--></span>');";
+					}
+					
+					for( $k=0; $i < count( $KeyArray ); $i++ ){
+						$keyJoin .= "jQuery('".$KeyArray[$k]."').before('<span><!--INFOLINKS_OFF--></span>');";
+						$keyJoin .= "jQuery('".$KeyArray[$k]."').after('<span><!--INFOLINKS_ON--></span>');";
+					}
+			   
+			   if( $JQuery == 1 ) {
+					$strOnOff = '<script type="text/javascript" src="'.esc_url( JS_PATH ).'"></script>';
+				}
+				
+				$strOnOff .= '<script type="text/javascript">
+							jQuery(document).ready(function(){ 
+							'.$cJoin.$idJoin.$keyJoin.'	
+								});
+						</script>';			   
+			   }
+	 }
+	
+	 if( $display == 1 ){
+	  $infoscript = '
+		<!-- Infolinks START -->
+		<script type="text/javascript">
+		   var infolink_pid ='.absint( get_option( 'infolinks_publisherid' ) ).'; var infolink_wsid = '.absint( get_option('infolinks_websiteid') ).';
+		</script>
+		<script type="text/javascript" src="http://resources.infolinks.com/js/infolinks_main.js"></script>
+		'.$strOnOff.'
+		<!-- Infolinks END -->';
+	 }
+	 echo $infoscript;
+	
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,109 @@
+===Infolinks Official Plugin===
+Contributors: Infolinks.com
+Donate link: http://www.infolinks.com/
+Tags: Infolinks, In Text Advertising, Website Advertising, Pay Per Click advertising, Contextual Advertising. 
+Requires at least: 2.8
+Tested up to: 3.2.1
+Stable tag: 3.0
+
+The Infolinks plugin enables WordPress Publishers to easily implement this CPC monetizing solution into WordPress sites.
+
+== Description ==
+
+Infolinks, the leading In-Text advertising provider, offers an official WordPress plugin. This plugin enables WordPress Publishers to easily implement this CPC monetizing solution into WordPress sites.
+
+What is Infolinks?
+
+Infolinks is a non-intrusive, content based advertising platform which enables website owners and bloggers alike to turn their content into money. Infolinks supports premium ads and because it is contextual advertising – readers are exposed to content relevant ads when they are most engaged.  Another perk of implementing In-Text ads is they don't require any additional website “real estate” and therefore leave more room for publisher’s content.
+Established in 2007 and backed by Primera Capital, Infolinks leads the industry with an attractive business model and guarantees the highest revenue share. Since the company's incorporation, tens of thousands of websites have integrated Infolinks In-Text ads.
+
+What are In-Text ads?
+
+In-Text Ads look just like regular hyperlinks accept they normally appear with a double underline (although Infolinks offers the option to customize the appearance of the links.)According to Infolinks’ smart algorithm, the highest paying keywords are double underlined and advertisers bid on these keywords in real-time. A simple hover of a mouse over one of these links and a highly relevant ad will then appear to visitors at the time when they are most engaged.
+
+Infolinks In-Text ads operate on a CPC model. Every time a visitor clicks on the ad bubble – they are redirected to the advertiser’s site and the publisher gets paid. It's that easy!
+Infolinks welcomes all WordPress publishers, big and small, to give this plugin a try and experience the next generation of In-Text advertising. There are no sign-up fees, hidden commitments or qualification minimums.
+
+== Installation ==
+
+1.	Download the Official Infolinks Plugin.
+2.	Activate the plugin through the 'Plugins' menu in WordPress
+3.	Visit the settings page under the Settings > Infolinks Settings submenu
+4.	Complete the short form with your account details and click Save Settings.
+5.	To find your account information, log in to your Infolinks account, click on Integration guide,choose your website, and copy your PID and website ID from the presented script to the module settings page.
+
+That's it - Infolinks In Text Ad links should appear on your website instantaneously.
+
+
+== Frequently Asked Questions ==
+
+= Why using In Text ads? =
+
+In Text advertising takes no extra space on your website since it simply uses the actual content of it, It is less intrusive since it doesn't distract from your website content, It is highly relevant for your site visitors so it generates high conversion rates and finally it is quick and easy to integrate with no changes to your website.
+
+= Why In Text Ads are more relevant? =
+
+While your website visitor reads an article, recognizes a term with double underline as an ad about a term which he is interested in, and wants to know a little more. He simply hovers with the mouse over the link and a bubble appears with an advertisement inside. He looks at the ad if he is interested; he can click on it and go directly to the advertiser's landing page. If he is not interested, he moves the mouse away and the bubble disappears.
+
+= What is the special Infolinks Highest Revenue Share Guarantee? =
+
+Simply, we guarantee the industry highest revenue share, so that you keep most of the advertising revenues. Infolinks is the next generation of In-Text advertising and with our proprietary algorithm and high quality of service, we are so confident that you will increase your advertising revenues, that we actually guarantee the industry highest revenue share in your contract.
+
+= What does Infolinks cost me? =
+
+Signing up with Infolinks is totally free. After a short integration process, each time your website visitors click on an Infolinks In-Text ad, you get paid. We seek the advertisers, you get the revenues, and we make sure you keep most of it with the industry highest revenue share offer. Join Infolinks now and you can qualify for payment today.
+
+= How does Infolinks in-text advertising work? =
+
+Infolinks is the next generation of In-Text advertising. After an effortless integration process, Infolinks intelligently scans your web pages using our dynamic proprietary algorithm and converts carefully selected keywords into cost-per-click (CPC) In-Text ads. Then, Infolinks automatically inserts highly relevant In-Text ads into your website’s content, leading to record high conversion rates. Each time your website visitors click on an In-Text ad, you get paid. We seek the advertisers, you get the revenues, and we make sure you keep most of it with the industry’s highest revenue share offer. Join Infolinks now and you can qualify for payment today.
+
+= How does Infolinks select its Advertisers? =
+
+Infolinks works with a variety of carefully selected Advertisers to ensure a wide range of relevant, quality ads for each content category.
+
+= Will the Infolinks be relevant to my site content? =
+
+Yes. Using our dynamic proprietary algorithm, Infolinks intelligently scans your web pages and converts carefully selected keywords into cost-per-click (CPC) In-Text ads. These specially formatted double underline hyperlinks lead to highly relevant advertiser content.
+
+Since the Infolinks In-Text ads are not visible unless triggered by the reader, Infolinks advertising is the least intrusive form of advertising and it does not compete with the websites content over the readers attention. Also, as the Infolinks In-Text ads ride on exiting text, it is the most relevant method of advertisement and the ads yield the highest interest from readers and accordingly, the highest conversions. Infolinks makes the benefits of In-Text advertising available to any website within a matter of minutes.
+
+= How do I earn money from Infolinks? =
+
+Each time your website visitors click on an In-Text ad, you get paid. We seek the advertisers, you get the revenues, and we make sure you keep most of it with the industry highest revenue share offer. Join Infolinks now and you can qualify for payment today.
+
+= How much can I earn through Infolinks advertising on my website? =
+
+After an effortless integration process, Infolinks automatically inserts highly relevant In-Text ads into your website content, leading to record high conversion rates. Each time your website visitors click on an In-Text ad, you get paid. We seek the advertisers, you get the revenues, and we make sure you keep most of it with the industry highest revenue share offer. The more clicks on your In-Text ads, the higher the revenue you earn. Therefore, to maximize your earnings, you should integrate Infolinks into all your viewed pages with optimized user experience.
+
+= How do I get paid? =
+
+Infolinks offers several convenient ways for publishers to receive payments. You can choose your preferred method: PayPal, Bank Wire, ACH (Only for U.S. bank accounts) or Get your Infolinks Prepaid Mastercard (Powered by Payoneer).
+
+= What is the Infolinks Tag Cloud? =
+
+Infolinks’ Tag Cloud uses our smart algorithm to create a colorful cloud of keywords which appear at the bottom of your content. It operates identically to our standard In-Text ads:  a simple mouse hover reveals our famous ad bubble containing top advertisements relevant to your cloud keywords.
+The Infolinks Tag Cloud can be added automatically, with only one click. Just follow this link and click the “Yes” checkbox and within 10 minutes, Tag Cloud will be added to all of your websites instantly. You can further customize the appearance of the Tag Cloud and which sites the Tag Cloud will appear.
+
+You can choose from our 4 different color layouts to customize your own Tag Cloud: Daylight, Evening, Seashore and Crystal. Find the best color scheme to compliment your website design for optimized monetization.
+
+[Learn more about the Infolinks Tag Cloud]()
+
+Learn more about howTag Cloud can boost your earnings.
+
+== Screenshots ==
+
+1. In Text Advertising Demo 
+2. Another In Text Demo
+
+== Changelog ==
+
+= 3.0 =
+* Complete rewrite of plugin
+* Hardened security measures
+* Updated for WordPress 3.2
+
+== Upgrade Notice ==
+
+= 3.0 =
+* Fixes multiple plugin bugs
+* Increased security
